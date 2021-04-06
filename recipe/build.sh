d4e526838a1fc1579f708f6aa59542f08557a0bd
Pavel Yakovlev <p_yak_11@mail.ru>
2021-04-06 12:16:31 +0300

    remove sudo

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 # create dpcpp environment
 function add_repo {
     wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
-    sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
+    apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
     rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
     echo "deb https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list
     add-apt-repository -y "deb https://apt.repos.intel.com/oneapi all main"
